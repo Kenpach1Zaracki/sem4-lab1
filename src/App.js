398257964631f2f0b269dfb40c11d2614bdfa792
Давыдов Максим <jabadadaya12@gmail.com>
2026-04-08 17:39:17 +0700

    Add authentication routes and private route handling

--- src/App.js
+++ src/App.js
@@ -1,20 +1,30 @@
 import React from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
 import Home from './pages/Home'
 import Detail from './pages/Detail'
 import Form from './pages/Form'
+import Login from './pages/Login'
+import Register from './pages/Register'
+import { isLoggedIn } from './auth'
+
+// Защищённый маршрут — если не залогинен, отправляет на /login
+const PrivateRoute = ({ children }) => {
+  return isLoggedIn() ? children : <Navigate to='/login' />
+}
 
 const App = () => {
-	return (
-		<Router>
-			<Routes>
-				<Route path='/' element={<Home />} />
-				<Route path='/detail/:id' element={<Detail />} />
-				<Route path='/add' element={<Form />} />
-				<Route path='/edit/:id' element={<Detail />} />
-			</Routes>
-		</Router>
-	)
+  return (
+    <Router>
+      <Routes>
+        <Route path='/login' element={<Login />} />
+        <Route path='/register' element={<Register />} />
+        <Route path='/' element={<PrivateRoute><Home /></PrivateRoute>} />
+        <Route path='/detail/:id' element={<PrivateRoute><Detail /></PrivateRoute>} />
+        <Route path='/add' element={<PrivateRoute><Form /></PrivateRoute>} />
+        <Route path='/edit/:id' element={<PrivateRoute><Detail /></PrivateRoute>} />
+      </Routes>
+    </Router>
+  )
 }
 
 export default App
